9299716f8989c6bbe882c6b762f53299039af0ff
Shu YAMANI <mail@shuymn.me>
2026-03-23 14:52:38 +0900

    fix(capsule): update version and URLs to v0.1.2

--- capsule.rb
+++ capsule.rb
@@ -1,24 +1,24 @@
 class Capsule < Formula
   desc 'Daemon-backed Rust prompt engine for zsh with async updates and hot-reloadable modules.'
-  version '0.1.1'
+  version '0.1.2'
   homepage 'https://github.com/shuymn/capsule'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/shuymn/capsule/releases/download/v0.1.1/capsule-v0.1.1-darwin-arm64.tar.gz'
-      sha256 'e915f2f467bc31aa36abfddb408732ef2a747d57d8d3237225db804e0ea256a5'
+      url 'https://github.com/shuymn/capsule/releases/download/v0.1.2/capsule-v0.1.2-darwin-arm64.tar.gz'
+      sha256 '44497e04237cc031d7d8346d0c3b9911bcaa3bbd7d17fe317d16f942e720e4aa'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/shuymn/capsule/releases/download/v0.1.1/capsule-v0.1.1-linux-arm64.tar.gz'
-      sha256 'bc823ff9b0c821391f088757816e65f5b15d411f83f957679a7b01db680aa41f'
+      url 'https://github.com/shuymn/capsule/releases/download/v0.1.2/capsule-v0.1.2-linux-arm64.tar.gz'
+      sha256 '979fcffe74bfe11938f3a4466218750dafccc37790016189c5d09f8a9015faa4'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/shuymn/capsule/releases/download/v0.1.1/capsule-v0.1.1-linux-amd64.tar.gz'
-      sha256 '6cb66d1b30ceadfb01290424a7ec9a059a5d75c4c1599257ab90b4c71590a266'
+      url 'https://github.com/shuymn/capsule/releases/download/v0.1.2/capsule-v0.1.2-linux-amd64.tar.gz'
+      sha256 '0b82d29001b946ef1a98f5e32ccfd4b52537832b1f80e2eb1548c8262541ea60'
     end
   end
 
@@ -26,14 +26,13 @@ class Capsule < Formula
     bin.install 'capsule'
   end
 
-  def post_install
-    system "#{bin}/capsule", "daemon", "install"
-  end
-
   def caveats
     <<~EOS
       To activate capsule, add the following to your ~/.zshrc:
         eval "$(capsule init zsh)"
+
+      To register the daemon, run:
+        capsule daemon install
 
       To unregister the daemon before uninstalling, run:
         capsule daemon uninstall
