57542c5a40a068da3100b51b0197c361fa65ade0
Shu YAMANI <mail@shuymn.me>
2026-05-20 21:13:45 +0900

    feat: update Kastty to v0.0.6

--- kastty.rb
+++ kastty.rb
@@ -1,27 +1,27 @@
 class Kastty < Formula
   desc 'A browser-based terminal sharing tool powered by ghostty-web and Bun'
-  version '0.0.5'
+  version '0.0.6'
   homepage 'https://github.com/shuymn/kastty'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/shuymn/kastty/releases/download/v0.0.5/kastty_v0.0.5_darwin_arm64.zip'
-      sha256 '190a10c79b9dc78d97cf954dc0ce39330b213fa026394f21b5684a07ae1e6551'
+      url 'https://github.com/shuymn/kastty/releases/download/v0.0.6/kastty_v0.0.6_darwin_arm64.zip'
+      sha256 'b33be493bdca091b6fbf6b466bb0185b671f7c36a6346eccc65fa9fa34cffd5c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/shuymn/kastty/releases/download/v0.0.5/kastty_v0.0.5_darwin_amd64.zip'
-      sha256 '00dfdb5f1306461bd82a96a162b72d8923e1f4b0959b3f2943f7d512f1c2a094'
+      url 'https://github.com/shuymn/kastty/releases/download/v0.0.6/kastty_v0.0.6_darwin_amd64.zip'
+      sha256 '83fae40514846761d66a20129f71e6677172e532d6d963136cc498bec16fa289'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/shuymn/kastty/releases/download/v0.0.5/kastty_v0.0.5_linux_arm64.tar.gz'
-      sha256 'c17cc0acd4d0397d3d9b7ccd0992afc55b9ef8afd6c70f22ba50b0d0f3631b1f'
+      url 'https://github.com/shuymn/kastty/releases/download/v0.0.6/kastty_v0.0.6_linux_arm64.tar.gz'
+      sha256 '53a1f37f0d3966d36da64d84fe3462c41c2b21bcee5b8b357ec2e94773350709'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/shuymn/kastty/releases/download/v0.0.5/kastty_v0.0.5_linux_amd64.tar.gz'
-      sha256 '4000ce52f3d0276cc61ed2dc84529a687d05e7df07d95c6e3242635d9a1c6d2f'
+      url 'https://github.com/shuymn/kastty/releases/download/v0.0.6/kastty_v0.0.6_linux_amd64.tar.gz'
+      sha256 'c655a46a19ec4af7ecde05ef8c507cb5cfe606361867b76562335646b5b38316'
     end
   end
 
